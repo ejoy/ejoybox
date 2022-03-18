8bd9a2a8836163b47dd734ccd8bf2348290d2eba
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2022-03-18 13:58:08 +0800

    fix

--- rsync_github.sh
+++ rsync_github.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+WORKDIR=/srv/s3intdev/ejoybox_github/ejoybox
+
+cd ${WORKDIR}
+
 rsync -av /srv/s3intdev/s3-i18n-publish/s3g_ob_merge_20211014/Newest/*.conf s3-i18n-publish/s3g_ob_merge_20211014/Newest/.
 git add .
 git commit -a -m "fix"
