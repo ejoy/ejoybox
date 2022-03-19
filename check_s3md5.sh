38ce25c33a6473f83eab8cbae1ca8fa3d40f80df
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2022-03-20 00:15:01 +0800

    fix

---
+++ check_s3md5.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_cn_3d/Newest/version_pc.conf -o /tmp/p10445_version_pc.conf
+curl -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_cn_3d/Newest/version_mobile.conf -o /tmp/p10445_version_mobile.conf
