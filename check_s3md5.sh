11a0f3fc56263e72cef2054f71a3f75b4636edc0
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2022-03-20 00:24:01 +0800

    fix

--- check_s3md5.sh
+++ check_s3md5.sh
@@ -3,8 +3,9 @@
 curl -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_cn_3d/Newest/version_pc.conf -o /tmp/p10445_version_pc.conf
 diff S3-CN-OB-Publish/ob_cn_3d/Newest/version_pc.conf /tmp/p10445_version_pc.conf
 if [ $? -ne 0 ] ; then
-	echo "close rewrite p10445 config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
-	sudo cp 
+	echo "close rewrite p10445_version_pc config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
+	sudo cp p10445-ob-hotfix.ejoy.com.conf_close /etc/nginx/sites-enabled/p10445-ob-hotfix.ejoy.com.conf
+	sudo nginx -s reload
 fi
 
 
@@ -12,5 +13,7 @@ fi
 curl -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_cn_3d/Newest/version_mobile.conf -o /tmp/p10445_version_mobile.conf
 diff S3-CN-OB-Publish/ob_cn_3d/Newest/version_mobile.conf /tmp/p10445_version_mobile.conf
 if [ $? -ne 0 ] ; then
-	echo "close rewrite p10445 config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
+	echo "close rewrite p10445_version_mobile config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
+	sudo cp p10445-ob-hotfix.ejoy.com.conf_close /etc/nginx/sites-enabled/p10445-ob-hotfix.ejoy.com.conf
+	sudo nginx -s reload
 fi
