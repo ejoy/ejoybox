af3422989f96a6172e28328b2e5886ccaefae447
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2022-03-20 00:21:01 +0800

    fix

--- check_s3md5.sh
+++ check_s3md5.sh
@@ -2,9 +2,15 @@
 
 curl -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_cn_3d/Newest/version_pc.conf -o /tmp/p10445_version_pc.conf
 diff S3-CN-OB-Publish/ob_cn_3d/Newest/version_pc.conf /tmp/p10445_version_pc.conf
-echo $?
+if [ $? -ne 0 ] ; then
+	echo "close rewrite p10445 config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
+	sudo cp 
+fi
+
 
 ##
 curl -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_cn_3d/Newest/version_mobile.conf -o /tmp/p10445_version_mobile.conf
 diff S3-CN-OB-Publish/ob_cn_3d/Newest/version_mobile.conf /tmp/p10445_version_mobile.conf
-echo $?
+if [ $? -ne 0 ] ; then
+	echo "close rewrite p10445 config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
+fi
