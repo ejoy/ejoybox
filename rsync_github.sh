55efb03d61c9cef25e33ee02885ac744c13dba11
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2023-05-31 14:15:01 +0800

    fix

--- rsync_github.sh
+++ rsync_github.sh
@@ -6,7 +6,6 @@ cd ${WORKDIR}
 
 git config --global push.default simple
 
-#rsync -av /srv/s3intdev/s3-i18n-publish/s3g_ob_merge_20211014/Newest/*.conf s3-i18n-publish/s3g_ob_merge_20211014/Newest/.
 rsync -av /var/www/html/S3-CN-OB-Publish/ob_v5/Newest/*.conf S3-CN-OB-Publish/ob_v5/Newest/.
 git add .
 git commit -a -m "fix"
