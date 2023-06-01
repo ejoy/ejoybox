6ce26d480be312338c45d1ce9fee4d4b132cf892
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2023-06-01 14:15:40 +0800

    230601141540:hear_no_evil:

--- rsync_github.sh
+++ rsync_github.sh
@@ -4,9 +4,10 @@ WORKDIR=/srv/s3intdev/ejoybox_github/ejoybox
 
 cd ${WORKDIR}
 
+TM=$(date +'%y%m%d%H%M%S')
+
 git config --global push.default simple
 
 rsync -av /var/www/html/S3-CN-OB-Publish/ob_v5/Newest/*.conf S3-CN-OB-Publish/ob_v5/Newest/.
-git add .
-git commit -a -m "fix"
+git commit -a -m "${TM}:hear_no_evil:"
 git push
