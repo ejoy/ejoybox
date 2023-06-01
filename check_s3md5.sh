1d545bd5a60d86eedf83a07eaec02cf07094e889
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2023-06-01 15:00:01 +0800

    230601150001:hear_no_evil:

--- check_s3md5.sh
+++ check_s3md5.sh
@@ -5,7 +5,7 @@ OnFlag="$1"
 
 cd ${WORKDIR}
 
-curl --connect-timeout 3 -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_v5/Newest/version.conf -o /tmp/p10445_version.conf
+time curl --connect-timeout 3 -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_v5/Newest/version.conf -o /tmp/p10445_version.conf
 curlack=$?
 diff S3-CN-OB-Publish/ob_v5/Newest/version.conf /tmp/p10445_version.conf
 diffack=$?
@@ -22,13 +22,3 @@ else
 		echo "version file redirect to github On at $(date)" >> /tmp/p10445_version_switch.log
 	fi
 fi
-
-
-##
-#curl -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_v5/Newest/version_mobile.conf -o /tmp/p10445_version_mobile.conf
-#diff S3-CN-OB-Publish/ob_v5/Newest/version_mobile.conf /tmp/p10445_version_mobile.conf
-#if [ $? -ne 0 ] ; then
-#	echo "close rewrite p10445_version_mobile config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
-#	sudo cp p10445-ob-hotfix.ejoy.com.conf_close /etc/nginx/sites-enabled/p10445-ob-hotfix.ejoy.com.conf
-#	sudo nginx -s reload
-#fi
