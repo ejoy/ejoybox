30580c92b97255cfe1687bc2f906803d32c122d3
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2024-03-05 17:57:08 +0800

    240305175708:hear_no_evil:

--- check_url.sh
+++ check_url.sh
@@ -2,11 +2,13 @@
 
 #source ./url.txt
 
-for url in $(cat url.txt)
+for url in $(cat url.txt | grep -v "^#")
 do
 echo "===${url}==="
 rm -rf /tmp/orgURL.txt
 time curl -s "${url}" -o /tmp/orgURL.txt
 md5sum /tmp/orgURL.txt
-
+echo
+echo "========NEXT========="
+echo
 done
