f9551181cfe446a24df8e5beb17175e7a5c5eda9
ali-hk-sre003 <ali-hk-sre003@ejoy.com>
2024-03-06 08:12:37 +0800

    240306081236:hear_no_evil:

--- check_s3md5.sh
+++ check_s3md5.sh
@@ -5,9 +5,11 @@ OnFlag="$1"
 
 cd ${WORKDIR}
 
-time curl --connect-timeout 3 -s https://raw.githubusercontent.com/ejoy/ejoybox/main/S3-CN-OB-Publish/ob_v5/Newest/version.conf -o /tmp/p10445_version.conf
+time curl --connect-timeout 3 -s https://pub-21098e95ad6f4b47a238727ff79a8b7b.r2.dev/S3-CN-OB-Publish/ob_v5/Newest/version.conf -o /tmp/p10445_version.conf
+time curl --connect-timeout 3 -s https://p10445-ob-hotfix-pp.ejoy.com/S3-CN-OB-Publish/ob_v5/Newest/version.conf -o /tmp/p10445_version_source.conf
 curlack=$?
-diff S3-CN-OB-Publish/ob_v5/Newest/version.conf /tmp/p10445_version.conf
+#diff S3-CN-OB-Publish/ob_v5/Newest/version.conf /tmp/p10445_version.conf
+diff /tmp/p10445_version_source.conf /tmp/p10445_version.conf
 diffack=$?
 if [ $curlack -ne 0 ] || [ $diffack -ne 0 ] ; then
 	echo "close rewrite p10445_version.config to nginx at $(date)" >> /tmp/p10445_hotfix_switch.log
